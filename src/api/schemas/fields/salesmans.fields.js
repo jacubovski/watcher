be8aa773ebe5bda20ecea2573529b8a603fa81f8
Breno Jacubovski <brenojacubovski@gmail.com>
2020-01-17 10:17:27 -0300

    Asstes folder for icon.png and Salesmans functions

--- src/api/schemas/fields/salesmans.fields.js
+++ src/api/schemas/fields/salesmans.fields.js
@@ -1,6 +1,6 @@
 const salesmanFetchFields = `vnd_codigo vnd_nome vnd_tipo_calculo vnd_situacao vnd_cota
 vnd_comissao_sem_cota vnd_comissao_com_cota vnd_telefone vnd_contato vnd_email
-vnd_endereco_endereco(company: $company) {
+vnd_endereco(company: $company) {
   end_origem end_codigo_origem end_tipo end_endereco end_numero end_complemento
   end_bairro end_cep end_uf end_cod_municipio end_municipio}
 `;
